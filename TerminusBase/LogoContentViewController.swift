ab1c2e686dd6871872a0a5ffb884d74bc80731d4
dr <duanrong01@gmail.com>
2017-01-03 23:47:42 +0800

    add logo layout

---
+++ TerminusBase/LogoContentViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LogoContentViewController.swift
+//  TerminusBase
+//
+//  Created by 段荣 on 2017/1/3.
+//  Copyright © 2017年 段荣. All rights reserved.
+//
+
+import Cocoa
+
+class LogoContentViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
