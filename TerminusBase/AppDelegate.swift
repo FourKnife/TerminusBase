a560ab336c43233ebd4cb94fe2299775d237435e
段荣 <dr@terminus.io>
2017-01-03 20:39:08 +0800

    Initial Commit

---
+++ TerminusBase/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TerminusBase
+//
+//  Created by 段荣 on 2017/1/3.
+//  Copyright © 2017年 段荣. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
