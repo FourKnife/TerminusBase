2570330eb935ddd58387480b9155c1a805b3321b
段荣 <dr@terminus.io>
2017-01-04 20:51:27 +0800

    add icon

--- TerminusBase/AppDelegate.swift
+++ TerminusBase/AppDelegate.swift
@@ -10,17 +10,17 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+  
+  @IBOutlet var statusItemMenu: NSMenu?
+  
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
 
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
+  }
+  
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+  
+  
 }
 
