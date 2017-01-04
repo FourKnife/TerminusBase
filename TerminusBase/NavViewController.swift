e0c0aa75a295edce6618941f4a0512421273ef8d
段荣 <dr@terminus.io>
2017-01-04 21:20:48 +0800

    add content split

---
+++ TerminusBase/NavViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NavViewController.swift
+//  TerminusBase
+//
+//  Created by 段荣 on 2017/1/4.
+//  Copyright © 2017年 段荣. All rights reserved.
+//
+
+import Cocoa
+
+class NavViewController: NSViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.wantsLayer = true
+  }
+  
+  override func awakeFromNib() {
+    if self.view.layer != nil {
+      let color : CGColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+      self.view.layer?.backgroundColor = color
+    }
+  }
+}
