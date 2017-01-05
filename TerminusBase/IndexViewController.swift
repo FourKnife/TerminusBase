bffca221844dff254ed79d3fe66dd7cac9f8935d
段荣 <dr@terminus.io>
2017-01-05 21:45:05 +0800

    add two content view

--- TerminusBase/IndexViewController.swift
+++ TerminusBase/IndexViewController.swift
@@ -1,27 +1,26 @@
 //
-//  NavViewController.swift
+//  IndexViewController.swift
 //  TerminusBase
 //
-//  Created by 段荣 on 2017/1/4.
+//  Created by 段荣 on 2017/1/5.
 //  Copyright © 2017年 段荣. All rights reserved.
 //
 
 import Cocoa
 
-class NavViewController: NSViewController {
+class IndexViewController: NSViewController {
   
-  @IBAction func test2Action(_ sender: Any) {
-    print("world2")
-  }
   override func viewDidLoad() {
     super.viewDidLoad()
+    // Do view setup here.
     self.view.wantsLayer = true
   }
   
   override func awakeFromNib() {
     if self.view.layer != nil {
-      let color : CGColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+      let color : CGColor = CGColor(red: 0, green: 1, blue: 0, alpha: 0.8)
       self.view.layer?.backgroundColor = color
     }
   }
+  
 }
--- TerminusBase/IssueViewController.swift
+++ TerminusBase/IssueViewController.swift
@@ -1,18 +1,15 @@
 //
-//  NavViewController.swift
+//  IssueViewController.swift
 //  TerminusBase
 //
-//  Created by 段荣 on 2017/1/4.
+//  Created by 段荣 on 2017/1/5.
 //  Copyright © 2017年 段荣. All rights reserved.
 //
 
 import Cocoa
 
-class NavViewController: NSViewController {
+class IssueViewController: NSViewController {
   
-  @IBAction func test2Action(_ sender: Any) {
-    print("world2")
-  }
   override func viewDidLoad() {
     super.viewDidLoad()
     self.view.wantsLayer = true
@@ -20,7 +17,7 @@ class NavViewController: NSViewController {
   
   override func awakeFromNib() {
     if self.view.layer != nil {
-      let color : CGColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+      let color : CGColor = CGColor(red: 1.0, green: 0, blue: 0, alpha: 0.8)
       self.view.layer?.backgroundColor = color
     }
   }
